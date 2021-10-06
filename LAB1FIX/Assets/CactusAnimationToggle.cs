e9e8272921f765bfa6d0cbc5ebe6f46fc0d48c61
unknown <marius.iurea@yahoo.com>
2021-10-06 18:45:13 +0300

    LAB1

---
+++ LAB1FIX/Assets/CactusAnimationToggle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CactusAnimationToggle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator animator;
+    private Animator otherAnimator;
+    public Transform otherObject;
+    public Text text;
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        otherAnimator = otherObject.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, otherObject.position);
+        animator.SetFloat("distance", distance);
+        otherAnimator.SetFloat("distance", distance);
+        text.text = distance.ToString();
+        
+    }
+}
